5c30605417fd21547158852e5edf4826be0f68ce
DungTNT <tuandungbrse@gmail.com>
2021-05-14 23:09:59 +0700

    first jsx & fixing eslint

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 
 import Pet from './Pet';
 
 const App = (props) => {
-  console.log(props);
-  return React.createElement(
-    'div',
-    {},
-    React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(
-      Pet,
-      { name: 'Ga Choi 1', animal: 'Ga', breed: 'Ga troi' },
-      '1'
-    ),
-    React.createElement(Pet, {
-      name: 'Khon Kiep',
-      animal: 'Cho',
-      breed: 'Cho Soi Bac Cuc',
-    }),
-    React.createElement(Pet, {
-      name: 'Con Lon',
-      animal: 'Su tu',
-      breed: 'Su Tu Chau Pho',
-    })
+  console.log('render in App: ' + props);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna 1" animal="Dog" breed="Asia" />
+      <Pet name="Luna 2" animal="Bird" breed="US" />
+      <Pet name="Luna 3" animal="Fish" breed="ES" />
+    </div>
   );
 };
 
-ReactDOM.render(
-  React.createElement(App, { id: 'hello word' }, 'hehehe'),
-  document.getElementById('root')
-);
+render(<App />, document.getElementById('root'));
--- src/Pet.js
+++ src/Pet.js
@@ -1,12 +1,23 @@
-import React from 'react';
+// import React from 'react';
+
+// const Pet = (props) => {
+//   console.log(props);
+//   return React.createElement('div', {}, [
+//     React.createElement('h2', {}, props.name),
+//     React.createElement('h3', {}, props.animal),
+//     React.createElement('h3', {}, props.breed),
+//   ]);
+// };
 
 const Pet = (props) => {
-  console.log(props);
-  return React.createElement('div', {}, [
-    React.createElement('h2', {}, props.name),
-    React.createElement('h3', {}, props.animal),
-    React.createElement('h3', {}, props.breed),
-  ]);
+  console.log('render in pet: ' + props);
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h3>{props.animal}</h3>
+      <h3>{props.breed}</h3>
+    </div>
+  );
 };
 
 export default Pet;
